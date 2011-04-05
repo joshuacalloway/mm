92d144ee50e59198b936264b49b51e3fe650d949
joshua calloway <joshua.calloway@gmail.com>
2011-04-04 22:05:28 -0500

    version 0.02

---
+++ MainGUI/MainGUI/EditRuleWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Rule rule { get; set; }
+        public void update()
+        {
+            comboBox2.Text = rule.name;
+        }
+        public Window1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            rule.market = Decimal.Parse(marketComboBox.Text);
+            this.Hide();
+        }
+    }
+}
---
+++ MainGUI/MainGUI/Rule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfApplication1
+{
+    public class Rule
+    {
+        public string name { get; set; }
+        public decimal market { get; set; }  // 0.05 or 0.10 cent wide
+    }
+}
