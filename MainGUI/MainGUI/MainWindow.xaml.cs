0ba091039432ec027cb9ae018e682406818f53bd
joshua calloway <joshua.calloway@gmail.com>
2011-04-06 14:08:19 -0500

    version 0.13

--- MainGUI/MainGUI/MainWindow.xaml.cs
+++ MainGUI/MainGUI/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +13,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using RealTick.Api.Domain.Livequote;
+using RealTick.Api.Application;
+using RealTick.Api.ClientAdapter;
+
+
 namespace WpfApplication1
 {
     /// <summary>
@@ -19,12 +25,57 @@ namespace WpfApplication1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private class TempForm : Form
+        {
+            LiveQuoteTable table;
+            ClientAdapterToolkitApp app;
+
+            public TempForm()
+            {
+                app = new ClientAdapterToolkitApp();
+                table = new LiveQuoteTable(app);
+                table.OnData += new EventHandler<RealTick.Api.Domain.DataEventArgs<LivequoteRecord>>(table_OnData);
+
+                table.OnLive += new EventHandler<EventArgs>(table_OnLive);
+                table.Start(this);
+                Console.WriteLine("TempForm() exit");
+            }
+
+            void table_OnLive(object sender, EventArgs e)
+            {
+                Console.WriteLine("CONNECTED OK");
+            }
+
+            // void table_OnDead(object sender, EventArgs e)
+            // {
+            //     Log("CONNECTION FAILED OR LOST");
+            // }
+
+            void table_OnData(object sender,  RealTick.Api.Domain.DataEventArgs<LivequoteRecord> e)
+            {
+                foreach (LivequoteRecord rec in e)
+                {
+                    if (rec.Trdprc1 != null)
+                        Console.WriteLine("TRADE {0}", rec.Trdprc1);
+                       // Log("TRADE {0}", rec.Trdprc1);
+                    if (rec.Bid != null)
+                        Console.WriteLine("BID {0} ASK {1}", rec.Bid, rec.Ask);
+                        //Log("BID {0} ASK {1}", rec.Bid, rec.Ask);
+                }
+            }
+
+        }
+
+        TempForm form;
+
+
         public MainWindow()
         {
             InitializeComponent();
 	    string appPath = System.IO.Path.GetDirectoryName(
 							     System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
 	    Rule.Source = new Uri(appPath + @"\Rules.xml");
+	    //	    form = new TempForm();
         }
 
 
@@ -35,10 +86,12 @@ namespace WpfApplication1
 	    Console.WriteLine("Save Button Clicked " + source);
 	}
 
-        private void ruleNameCombBox_KeyDown(object sender, KeyEventArgs e)
+        private void ruleNameCombBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
             Console.WriteLine("comboBox2_KeyDown");
            // editRuleButton.IsEnabled = true;
         }
+
+
     }
 }
--- MainGUI/MainGUI/Properties/Resources.Designer.cs
+++ MainGUI/MainGUI/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MMApp.Properties {
+namespace mm.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MMApp.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MMApp.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("mm.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
