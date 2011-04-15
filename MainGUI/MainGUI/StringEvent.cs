44896ea6fea80edde0b5d3f44f68d3093d8d4e77
joshua calloway <joshua.calloway@gmail.com>
2011-04-15 11:21:45 -0500

    version 0.28

---
+++ MainGUI/MainGUI/StringEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mm
+{
+    class StringEvent : RealTick.Api.Domain.ExtensibleRecord
+    {
+        private string msg;
+        public StringEvent() { }
+        public StringEvent(string msg) { this.msg = msg; }
+        public string Msg
+        {
+            get { return msg; }
+            set { msg = value; }
+        }
+    }
+}
